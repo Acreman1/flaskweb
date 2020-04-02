f4e45eaf6f4a8d1fa3ae6a714e935a1df3cf6b67
Acreman <acreman@sina.com>
2020-04-02 16:15:02 +0800

    提交数据库

--- one/watchlist/app.py
+++ one/watchlist/app.py
@@ -1,7 +1,32 @@
-from flask import Flask,url_for,render_template
+import os,sys
 
+from flask import Flask,url_for,render_template
+from flask_sqlalchemy import SQLAlchemy
+import click
+
+WIN = sys.platform.startswith('win')
+if WIN:
+    prefix = "sqlite:///"  # windows平台
+else:
+    prefix = "sqlite:////"  # Mac,linux平台
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path,'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+# models 数据层
+class User(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(24))
+
+class Movie(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    title = db.Column(db.String(64))
+    year = db.Column(db.String(4))
+
+# views 视图函数
 @app.route('/')
 def index():
     name = "Acreman"
@@ -13,3 +38,16 @@ def index():
         {"title":"复仇者联盟4","year":"2019"},
     ]
     return render_template("index.html",name=name,movies=movies)
+
+
+
+# 自定义命令
+@app.cli.command()  # 注册为命令
+@click.option('--drop',is_flag=True,help="先删除再创建")
+def initdb(drop):
+    if drop:
+        db.drop_all()
+    db.create_all()
+    click.echo('初始化数据库')
+
+
