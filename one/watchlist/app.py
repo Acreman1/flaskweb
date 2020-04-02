9223e576c852b23097eaa32db091e3c3abb4d27e
Acreman <acreman@sina.com>
2020-04-02 09:25:30 +0800

    管理环境变量和url

--- one/watchlist/app.py
+++ one/watchlist/app.py
@@ -3,5 +3,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/index')
+@app.route('/dome')
 def index():
     return "hello word"
+
+
+@app.route('/user/<name>')
+def user(name):
+    return "hello %s" %name
