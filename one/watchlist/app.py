f3040a82bd6478555f0ccd9534300f6e0049a2a1
Acreman <acreman@sina.com>
2020-04-02 09:53:10 +0800

    添加了模板

--- one/watchlist/app.py
+++ one/watchlist/app.py
@@ -1,14 +1,15 @@
-from flask import Flask
+from flask import Flask,url_for,render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/index')
-@app.route('/dome')
 def index():
-    return "hello word"
-
-
-@app.route('/user/<name>')
-def user(name):
-    return "hello %s" %name
+    name = "Acreman"
+    movies = [
+        {"title":"战狼2","year":"2019"},
+        {"title":"速度与激情8","year":"2018"},
+        {"title":"叶问4","year":"2020"},
+        {"title":"南方车站","year":"2019"},
+        {"title":"复仇者联盟4","year":"2019"},
+    ]
+    return render_template("index.html",name=name,movies=movies)
