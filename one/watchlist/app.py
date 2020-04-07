108539924fff4c63ca40b126390354f31b5334c0
Acreman <acreman@sina.com>
2020-04-07 14:51:47 +0800

    对数据的更新和删除

--- one/watchlist/app.py
+++ one/watchlist/app.py
@@ -46,6 +46,31 @@ def index():
     movies = Movie.query.all()
     return render_template("index.html",movies=movies)
 
+@app.route('/movie/edit/<int:movie_id>',methods=['GET','POST'])
+def edit(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+    if request.method == "POST":
+        title = request.form.get("title")
+        year = request.form.get("year")
+        if not title or not year or len(year)>4 or len(title)>60:
+            flash('输入有误')
+            return redirdect(url_for('edit'),movie_id=movie_id)
+        movie.title = title
+        movie.year = year
+        db.session.commit()
+        flash("电影更新完成")
+        return redirect(url_for("index"))
+    context = {"movie":movie}
+    return render_template("edit.html",**context)
+
+
+@app.route('/movie/delete/<int:movie_id>',methods=['GET','POST'])
+def delete(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+    db.session.delete(movie)
+    db.session.commit()
+    flash("删除完成")
+    return redirect(url_for('index'))
 
 
 # 自定义命令
